d8c93c86cc71147868e3e5c4848f3ae3c911b13b
Violet28-tbc <u3644077@connect.hku.hk>
2025-11-24 00:41:28 +0800

    Remove unnecessary blank line in main.cpp

---
+++ main.cpp
@@ -0,0 +1,13 @@
+#include "game.h"
+#include <iostream>
+using namespace std;
+
+int main() {
+    cout << "Starting HKU GPA Escape..." << endl;
+    
+    Game game;
+    game.run();
+    
+    cout << "Thank you for playing HKU GPA Escape!" << endl;
+    return 0;
+}
